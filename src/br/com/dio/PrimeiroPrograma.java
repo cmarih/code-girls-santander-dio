ed4269a0b0a517deec184a5a79eaf51b914ef6b2
cmarih <marycostajc@gmail.com>
2022-07-17 20:16:33 -0300

    Adicionando primeiros arquivos ao repositorio.

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -4,7 +4,7 @@ import br.com.dio.model.Gato;
 
 public class PrimeiroPrograma {
     public static void main(String[] args) {
-        Gato gato = new Gato();
+        Gato gato = new Gato("Zoe", "pintado", "2");
         System.out.println(gato);
 //        System.out.println("Hello World!!");
     }
